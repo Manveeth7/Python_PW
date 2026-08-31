b014b2882025cf35fc4fefbdb692b27e5aeb4d05
Manveeth7 <manveethtb7@gmail.com>
2026-08-31 08:43:07 +0000

    Commit 05 Memory Script

---
+++ memory.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+
+
+mem=$(free -h|awk 'NR>1{print $3+0}')
+#echo "$mem"
+for memo in $mem; do
+	[[ $memo -gt 100 ]] && echo "$memo -> High Memory consumption" || echo " $memo -> Healthy"
+	
+done
